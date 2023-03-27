0e55d580e5d36c5ed13571881be9ecddf0beaa32
Felipe Kenji Yunoki <87314665+FelipeYunoki@users.noreply.github.com>
2023-03-27 10:53:54 -0300

    melhoria

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React from 'react-native';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 
@@ -22,48 +22,48 @@ export default function Register({navigation}) {
                     colors={['#FFC15E', '#CE6A85']}>
                 </LinearGradient>
             </View>
+            <ScrollView>
+                <View style={styles.form}>
+                    <Text style={styles.h1}>Cadastra-se</Text>
+                    
+                    <View style={styles.group}>
+                        <Text style={styles.p}>Nome completo</Text>
+                        <TextInput style={styles.input}/>
+                    </View>
 
-            <View style={styles.form}>
-                <Text style={styles.h1}>Cadastra-se</Text>
-                
-                <View style={styles.group}>
-                    <Text style={styles.p}>Nome completo</Text>
-                    <TextInput style={styles.input}/>
+                    <View style={styles.group}>
+                        <Text  style={styles.p}>Nome do usuário</Text>
+                        <TextInput style={styles.input}/>
+                    </View> 
+
+                    <View style={styles.group}>
+                        <Text  style={styles.p}>E-mail</Text>
+                        <TextInput style={styles.input}/>
+                    </View>
+
+                    <View style={styles.group}>
+                        <Text  style={styles.p}>Senha</Text>
+                        <TextInput style={styles.input}/>
+                    </View>
+
+                    <View style={styles.group}>
+                        <Text  style={styles.p}>confirmação de Senha</Text>
+                        <TextInput style={styles.input}/>
+                    </View>
+
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Cadastrar</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.opacity} onPress={()=>{navigation.navigate('Login')}}>
+                        <Text style={styles.link}>Já possuo uma conta</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.opacity}>
+                        <Text style={styles.link}>Sou um vendedor</Text>
+                    </TouchableOpacity>
                 </View>
-
-                <View style={styles.group}>
-                    <Text  style={styles.p}>Nome do usuário</Text>
-                    <TextInput style={styles.input}/>
-                </View> 
-
-                <View style={styles.group}>
-                    <Text  style={styles.p}>E-mail</Text>
-                    <TextInput style={styles.input}/>
-                </View>
-
-                <View style={styles.group}>
-                    <Text  style={styles.p}>Senha</Text>
-                    <TextInput style={styles.input}/>
-                </View>
-
-                <View style={styles.group}>
-                    <Text  style={styles.p}>confirmação de Senha</Text>
-                    <TextInput style={styles.input}/>
-                </View>
-
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Cadastrar</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.opacity} onPress={()=>{navigation.navigate('Login')}}>
-                    <Text style={styles.link}>Já possuo uma conta</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.opacity}>
-                    <Text style={styles.link}>Sou um vendedor</Text>
-                </TouchableOpacity>
-
-            </View>
+            </ScrollView>
 
             <View style={styles.top}>
                 <LinearGradient style={styles.circulo}
